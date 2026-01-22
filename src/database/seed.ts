01feb70ae8523a387d217ee2677fc3dbae2f6f7d
Abhiyan Shrestha <iabhiyaan@gmail.com>
2026-01-22 21:24:32 +0545

    chore: use dbconfig instead

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -1,13 +1,16 @@
 import { DataSource } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import databaseConfig from '../config/database.config';
+
+const config = databaseConfig();
 
 const dataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME || '',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || '',
+  host: config.host,
+  port: config.port,
+  username: config.username,
+  password: config.password,
+  database: config.name,
   synchronize: false,
   logging: false,
 });
